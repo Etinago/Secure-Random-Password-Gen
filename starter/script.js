3ad6347c90d99d562d22bc05d3ab04449887462c
Etinago <124711424+Etinago@users.noreply.github.com>
2023-07-25 23:40:52 +0100

    made changes to create a unique password

--- starter/script.js
+++ starter/script.js
@@ -1,96 +1,22 @@
 // Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
+var specialCharacters = "!£$%^&*()_-+{}+[]?>\</|";
+console.log(specialCharacters);
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = "0123456789"
+console.log(numericCharacters)
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
+var lowerCasedCharacters = "abcdefghijklmnopqrstuvwxyz"
+console.log(lowerCasedCharacters.length);
 
 // declare the password length 
 
 var passwordLength = 12; 
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
+var upperCasedCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+console.log(upperCasedCharacters.length);
 
 // Function to prompt user for password options
 function getPasswordOptions() {
@@ -102,31 +28,35 @@ function getPasswordOptions() {
 
 //}
 
-allChars = upperCasedCharacters + lowerCasedCharacters + numericCharacters + specialCharacters;
+// creating array with all characters 
+var allChars = upperCasedCharacters + lowerCasedCharacters + numericCharacters + specialCharacters;
+console.log(allChars);
 
 // Function to generate password with user input
 function generatePassword() {
-  let password = "";
-  password += upperCasedCharacters[Math.floor(Math.random() * upperCasedCharacters.length)];
-  password += lowerCasedCharacters[Math.floor(Math.random() * lowerCasedCharacters.length)];
-  password += numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
+  const securePassword = "";
+  securePassword += upperCasedCharacters[Math.floor(Math.random() * upperCasedCharacters.length)];
+  securePassword += lowerCasedCharacters[Math.floor(Math.random() * lowerCasedCharacters.length)];
+  securePassword += numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
+  securePassword += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
 
-  while(length > password.length){
-    password =+ allChars[Math.floor(Math.random() * allChars.length)]
+  while(password.length < passwordLength){
+    securePassword =+ allChars[Math.floor(Math.random() * allChars.length)];
   }
-  console.log(password)
 }
+
+passwordBox.value = securePasswordpassword;
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+//var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
-}
+//   passwordText.value = password;
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener('click', writePassword);
